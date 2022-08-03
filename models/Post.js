0f5b2861a3a03b935df637b2ebce00f16af90617
Sean Oh <seanoh@ucsb.edu>
2022-08-03 02:27:08 -0700

    add Post model

---
+++ models/Post.js
@@ -0,0 +1,39 @@
+const {Model, DataTypes} = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model{};
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    modelName: 'post',
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true
+  }
+);
+
+module.exports = User;
