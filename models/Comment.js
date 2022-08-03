62d1fa17f9aecaf18cf19f75792a2d7354a05bfa
Sean Oh <seanoh@ucsb.edu>
2022-08-03 02:28:30 -0700

    add Comment model

---
+++ models/Comment.js
@@ -0,0 +1,42 @@
+const {Model, DataTypes} = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model{};
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    modelName: 'comment',
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true
+  }
+);
+
+module.exports = User;
